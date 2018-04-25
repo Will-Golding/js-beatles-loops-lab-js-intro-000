6fb192ae543f7e08d3aa214e063b1ea8ebf9f8c6
will Golding <willgolding117@gmail.com>
2018-04-25 13:56:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments) {
   var a = [];
   for(i = 0; i < musicians.length; i++ ){
     a.push(musicians[i] + "plays" + instruments[i]);
   }
   return a;
+}
+
+function johnLennonFacts(facts) {
+  var updatedFacts = [];
+  var i = 0;
+  while(i< facts.length){
+    updatedFacts.push(facts[i] + "!!!")
+  }
 }
